6b7f7949020d59fe31bb9d845e7a19e7178cdc72
dheerajkumarreddykotha <dheerajkumarreddykotha@gmail.com>
2023-08-01 21:48:41 -0500

    Adding Math learning

---
+++ exercises/math.py
@@ -0,0 +1,14 @@
+# Math Module - Python has built in Math module to perform all mathematical operations
+
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+x = abs(-5)
+print(x)
+
+# To learn more refer to math module which has more such functions
